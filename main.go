04e0f2cfe9a9b9462b122c465f435c00bfe43064
Giang Le Ngoc <gianglengoc.hut@gmail.com>
2017-09-28 18:27:38 +0700

    update

--- main.go
+++ main.go
@@ -1,46 +1,89 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"log"
-
 	"time"
 
 	"github.com/streadway/amqp"
 )
 
-var amqpUri = "amqp://guest:guest@localhost:5672/"
-var rabbitCloseError chan *amqp.Error
-
-type RabbitMQ struct {
+type RabbimtMQ struct {
+	conn  *amqp.Connection
+	chann *amqp.Channel
+	done  chan error
 }
 
-func main() {
-	for {
-		fmt.Println("Begin create new connection to rabbitmq in 5 second.")
-		time.Sleep(5 * time.Second)
+var rabbit *RabbimtMQ
 
-		conn := connect()
-		//done := make(chan error)
-
-		go func() {
-			log.Printf("closing here %s", <-conn.NotifyClose(make(chan *amqp.Error)))
-			//done <- errors.New("Channel Close")
-			conn = connect()
-		}()
-
-		log.Println("got connection, creating channel")
+func NewRabbitMQ() *RabbimtMQ {
+	return &RabbimtMQ{
+		done: make(chan error),
 	}
 }
 
-func connect() *amqp.Connection {
+func initRabbitMQ() {
+	rabbit = NewRabbitMQ()
+
+	var err error
+
+	rabbit.conn, err = amqp.Dial("amqp://guest:guest@localhost:5672/")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	rabbit.chann, err = rabbit.conn.Channel()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	go func() {
+		log.Printf("closing: %s", <-rabbit.conn.NotifyClose(make(chan *amqp.Error)))
+		rabbit.done <- errors.New("Channel Closed")
+	}()
+
+	fmt.Println("Connect rabbitmq completed")
+
+}
+
+func main() {
+	initRabbitMQ()
+	startChan := make(chan int)
+
+	go sendingRequest(rabbit, startChan)
+
+	for i := 0; i < 100000; i++ {
+		time.Sleep(1 * time.Second)
+		startChan <- i
+	}
+
+}
+
+func sendingRequest(rabbit *RabbimtMQ, startChan chan int) {
+	fmt.Println("Sending request")
+	pause := false
+	var err error
 	for {
-		conn, err := amqp.Dial(amqpUri)
-		if err == nil {
-			return conn
+		if !pause {
+			select {
+			case i := <-startChan:
+				fmt.Println("Start :", i)
+			case notifi := <-rabbit.done:
+				fmt.Printf("Begin re-connect rabbitmq: %v \n", notifi)
+				for {
+					// auto reconnect in 3 seconds
+					time.Sleep(3 * time.Second)
+					rabbit.conn, err = amqp.Dial("amqp://guest:guest@localhost:5672/")
+					if err != nil {
+						fmt.Printf("Re-connect rabbitmq failed: %v \n", err)
+						continue
+					}
+					fmt.Printf("Re-connect rabbitmq succesfull \n", nil)
+					pause = false
+					break
+				}
+			}
 		}
-		log.Println(err)
-		log.Println("Trying to reconnect to rabbitmq in 2 seconds...")
-		time.Sleep(2 * time.Second)
 	}
 }
